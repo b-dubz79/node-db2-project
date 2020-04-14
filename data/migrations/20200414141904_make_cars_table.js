107fe8577acc5118ddc3f37cb3168a226f6efe4c
unknown <bwardwell79@gmail.com>
2020-04-14 15:28:33 -0700

    Met MVP

---
+++ data/migrations/20200414141904_make_cars_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments();
+      tbl.string('VIN', 128).notNullable()
+      tbl.string('make').notNullable()
+      tbl.string('model').notNullable()
+      tbl.decimal('mileage').notNullable()
+      tbl.string('transmission_type')
+      tbl.string('title_status')
+
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars')
+};
